6e536d7e1b1ce645dbc1973016ca1ce204a29378
mahmoudali15 <130462575+mahmoudali15@users.noreply.github.com>
2024-03-19 09:26:07 +0200

    create_fontfamiles_andfontweigth

---
+++ lib/core/style/fonts/font_famaily_helper.dart
@@ -0,0 +1,6 @@
+class FontFamilyHelper {
+  const FontFamilyHelper._();
+  static const String cairoAribic = 'Cairo';
+  static const String cairoEnglish = 'Poppins';
+  //switch font
+}
--- lib/store_app.dart
+++ lib/store_app.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:store/core/app/connective_controller.dart';
 import 'package:store/core/common/screen/no_network.dart';
+import 'package:store/core/style/fonts/font_famaily_helper.dart';
+import 'package:store/core/style/fonts/font_weigth_hlper.dart';
 
 class StoreApp extends StatelessWidget {
   const StoreApp({super.key});
@@ -12,26 +15,59 @@ class StoreApp extends StatelessWidget {
       valueListenable: ConnectiveController.instance.isConnective,
       builder: (_, value, __) {
         if (value) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Flutter Demo',
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
-            ),
-            builder: (context, widget) {
-              return Scaffold(
-                body: Builder(
-                  builder: (context) {
-                    ConnectiveController.instance.init();
-                    return widget!;
-                  },
+          return ScreenUtilInit(
+            designSize: const Size(375, 812),
+            minTextAdapt: true,
+            child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+              ),
+              builder: (context, widget) {
+                return Scaffold(
+                  body: Builder(
+                    builder: (context) {
+                      ConnectiveController.instance.init();
+                      return widget!;
+                    },
+                  ),
+                );
+              },
+              home: const Scaffold(
+                body: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Flutter Demo Home Page',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'مرحبا بك في التطبيق',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                        'Flutter Demo Home Page',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeightHelper.bold,
+                          fontFamily: FontFamilyHelper.cairoEnglish,
+                        ),
+                      ),
+                      Text(
+                        'مرحبا بك في التطبيق',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeightHelper.bold,
+                          fontFamily: FontFamilyHelper.cairoAribic,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              );
-            },
-            home: const Scaffold(
-              body: Center(
-                child: Text('Flutter Demo Home Page'),
               ),
             ),
           );
